9a73ba99208eb4ed3e68605033927cf489151c88
SJimenezB <santijimenezbonilla@gmail.com>
2018-06-06 05:02:21 -0500

    Ajustes estructura y modelo

--- apiTWP/src/estructura/interfaces/IRecaudo.java
+++ apiTWP/src/estructura/interfaces/IRecaudo.java
@@ -1,8 +1,13 @@
 package estructura.interfaces;
 
+import java.util.Hashtable;
+
+import estructura.modelos.Factura;
+
 public interface IRecaudo {
 	
 	void captarPago(int monto, int idUsuario, int idEspacio, int idTarjeta);
 	void crearTarjeta(int id, int idCliente, String codigoBarras);
+	Hashtable<Integer, Factura> obtenerListaFacturas();
 	
 }
---
+++ apiTWP/src/estructura/modelos/Factura.java
@@ -0,0 +1,53 @@
+package estructura.modelos;
+
+import java.util.Date;
+
+import estructura.modelos.Espacio;
+import estructura.modelos.Usuario;
+
+public abstract class Factura {
+
+	private int id;
+	private int idUsuario;
+	private int idEspacio;
+	private double valor;
+	private Date fecha;
+	private int tiempo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public int getIdEspacio() {
+		return idEspacio;
+	}
+	public void setIdEspacio(int idEspacio) {
+		this.idEspacio = idEspacio;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public int getTiempo() {
+		return tiempo;
+	}
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+	
+}
